af804e2e785ad975bf023f10b20c877e11d4ea92
Yue Wang <yuewang0809@Yues-MBP.fios-router.home>
2020-11-14 00:25:15 -0800

    hw

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,37 @@
 jQuery(function() {
 
+	$(document).ready(function(){
+	  $("#p1").mouseenter(function(){
+	    alert("I know you are ready to leave 2020 behind");
+	  });
+	});
+
+  let my_switch = false;
+
 	$('#my_button').click(function () {
+
+		if ( my_switch == false ){
+		  my_switch = true;
+
+			$(this).html('I AM READY!!');
+
+		  $('#my_image').attr('src', 'images/alone.gif');
+		} else {
+		  my_switch = false;
+
+			$(this).html('I rather be alone...');
+
+		  $('#my_image').attr('src', 'images/party-1.gif');
+		}
+
+		//if ( $('.square.alt').hasClass('red') ){
+		//     $('.square.alt').removeClass('red');
+		//}else {
+		//     $('.square.alt').addClass('red');
+		//}
+
+
+
 	});
 
 });
